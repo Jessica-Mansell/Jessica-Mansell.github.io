66d9a13d5dc7cc1cfa2e0175a7941a4bc06049b8
Jessica Mansell <jcmansell88@gmail.com>
2021-12-07 17:08:14 -0600

    creating variables

--- Python/pythonSyntaxPractice.py
+++ Python/pythonSyntaxPractice.py
@@ -6,3 +6,12 @@ if 5 > 2:
     print("Five is greater than two!")
 #This is a comment.
 print("Hello, World!")
+
+#This is a multi-
+#line comment
+#because there is no other way
+
+x = 5
+y = "John"
+print(x)
+print(y)
