5ced5a550e289d3e6a3f9e4cbd4b911ef3811d14
Jessica Mansell <jcmansell88@gmail.com>
2021-12-07 17:12:12 -0600

    casting 3

--- Python/pythonSyntaxPractice.py
+++ Python/pythonSyntaxPractice.py
@@ -11,15 +11,10 @@ print("Hello, World!")
 #line comment
 #because there is no other way
 
-x = 5
-y = "John"
-print(x)
-print(y)
-
-x = 4
-x = "Sally"
-print(x)
-
 x = str(3)
 y = int(3)
 z = float(3)
+
+print(x)
+print(y)
+print(z)
