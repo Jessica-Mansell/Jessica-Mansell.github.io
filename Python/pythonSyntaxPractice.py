de0c08c83dc5bc71def96abcfa2400500bab9aeb
Jessica Mansell <jcmansell88@gmail.com>
2021-12-10 15:40:08 -0600

    converting types

--- Python/pythonSyntaxPractice.py
+++ Python/pythonSyntaxPractice.py
@@ -170,3 +170,25 @@ z = -5j
 print(type(x))
 print(type(y))
 print(type(z))
+
+#converting one type to another
+x = 1
+y = 2.8
+z = 1j
+
+#convert from int to float
+a = float(x)
+
+#convert from float to int
+b = int(y)
+
+#convert from int to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
