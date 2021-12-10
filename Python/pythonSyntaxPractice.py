1b50f9ac512c377285ded31c71a14d3402eb7352
Jessica Mansell <jcmansell88@gmail.com>
2021-12-10 15:36:25 -0600

    complex number example

--- Python/pythonSyntaxPractice.py
+++ Python/pythonSyntaxPractice.py
@@ -161,3 +161,12 @@ z = -87.7e100
 print(type(x))
 print(type(y))
 print(type(z))
+
+#Complex numbers are written with a "j" as the imaginary part:
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
