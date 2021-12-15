accffef63e5692eef3954d9eb4e73507c7371341
Jessica Mansell <jcmansell88@gmail.com>
2021-12-14 18:43:40 -0600

    check if NOT

--- Python/pythonSyntaxPractice.py
+++ Python/pythonSyntaxPractice.py
@@ -240,3 +240,11 @@ print("free" in txt)
 txt = "The best things in are free!"
 if "free" in txt:
     print("Yes, 'free' is present.")
+
+#To check if a certain phrase or character is NOT present in a string, we can use the keyword not in.
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+#Use it in an if statement
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
