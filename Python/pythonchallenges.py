18073d952ea673d146390c72fde64233f7ffab99
Jessica Mansell <jcmansell88@gmail.com>
2021-12-30 18:51:17 -0600

    created new python file for challenges

---
+++ Python/pythonchallenges.py
@@ -0,0 +1,13 @@
+#creating for loop to return numbers 1-10
+for num in range(1,11):
+    print(num)
+
+#creating for loop to return numbers 1-10 in reverse
+for num in range(11, 0, -1):
+  print(num)
+
+#repeat 1-3 3x
+for num in range(1, 4):
+    for num in range(1, 4):
+        for num in range(1, 4):
+            print(num)
