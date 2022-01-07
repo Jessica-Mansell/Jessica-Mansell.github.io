b8cdcb011262edfc04561a799bac3ea944a24ad7
Jessica Mansell <jcmansell88@gmail.com>
2022-01-06 22:23:09 -0600

    bubbleSort, selection Sort

--- Python/pythonchallenges.py
+++ Python/pythonchallenges.py
@@ -14,3 +14,27 @@ for num in range(1, 4):
 for x in range(1,4):
     for y in range(1,4):
         print("Iteration is", x, "and y is", y)
+
+def bubbleSort(input):
+    for i in range(len(input)):
+        for j in range(len(input)-1):
+            if input[j] > input[j + 1]:
+                #swap (input, j, j+1)
+                tmp = input[j]
+                input[j] = input[j + 1]
+                input[j + 1] = tmp
+    return input
+
+print(bubbleSort([6,4,8,4,6,4,1,1,6,5,1,6,5,4]))
+
+def selectionSort(input):
+    for i in range(len(input)):
+        for j in range(len(input)):
+            if input[i] < input[j]:
+                #swap (input, j, j+1)
+                tmp = input[i]
+                input[i] = input[j]
+                input[j] = tmp
+    return input
+
+print(selectionSort([6,4,8,4,6,4,1,1,6,5,1,6,5,4]))
